a8b5e56db5c4cc50ada0014f5a5576d899a46aea
samir sayyed <sayyedsamir11@gmail.com>
2021-12-18 20:22:09 +0530

    style added

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
-
 router.get('/', homeController.home);
+
+
 router.use('/projects', require('./projects'));
 
 
